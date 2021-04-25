6c49e4d8bb3cf33803b5dea208c9ba9a2a470e24
jinsu1 <jinsu4205@gmail.com>
2021-04-25 18:42:54 +0900

    chat

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const path = require("path");
 const server = http.createServer(app);
 const socketIO = require("socket.io");
+const moment = require("moment");
 
 const io = socketIO(server);
 
@@ -14,7 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
     socket.on("chatting", (data) => {
-        io.emit("chatting", data) 
+        const { name, msg } = data;
+        io.emit("chatting", { 
+            name,
+            msg,
+            time: moment(new Date()).format("h:mm A")
+        })
     })
 })
 
